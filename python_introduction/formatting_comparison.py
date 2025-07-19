41b865d2d9f167063973f7797e7f9195bf517c39
R4Rapha <theraphaelidemetuk@gmail.com>
2025-07-19 23:47:30 +0100

    Updated Codebase

---
+++ python_introduction/formatting_comparison.py
@@ -0,0 +1,14 @@
+# Variables
+name = "Raphael"
+age = 25
+savings = 10500.756
+
+# Basic print with commas
+print("Hello", name + "!")
+print("You are", age, "years old.")
+print("Your current savings are $", savings)
+
+# Same thing using f-strings
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
+print(f"Your current savings are ${savings:.2f}")
